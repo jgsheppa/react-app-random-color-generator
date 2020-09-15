bd3a64c219fed223af337618e8b33dd42f7c675d
James Sheppard <jamessheppard@Verenas-MBP.public.ph.local>
2020-09-15 17:32:15 +0200

    Added height and width options, redesigned site

--- src/App.js
+++ src/App.js
@@ -6,30 +6,59 @@ import randomColor from 'randomcolor';
 function App() {
   const [textColor, setTextColor] = React.useState('#FFFFFF');
   const { register, handleSubmit } = useForm();
+  const [boxWidth, setBoxWidth] = React.useState('100px');
+  const [boxHeight, setBoxHeight] = React.useState('100px');
 
   const onSubmit = (data) => {
     console.log(data);
-    setTextColor(randomColor(data));
+    setTextColor(
+      randomColor({
+        hue: data.hue,
+        luminosity: data.luminosity,
+      }),
+    );
+    console.log(setTextColor(randomColor(data)));
+    setBoxWidth(data.width);
+    console.log(setBoxWidth(data));
+    setBoxHeight(data.height);
+    console.log(setBoxHeight(data));
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="flexbox">
-          <div className="background-box">
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flexbox background-box">
+            <div className="container">
               <label className="item">Enter a primary color</label>
-              <input className="item" ref={register} name="hue" />
-
-              <label className="item">Enter a shade (e.g. light, dark)</label>
-              <input className="item" ref={register} name="luminosity" />
-              <button className="button item">Submit Color</button>
-            </form>
+              <input className="itemInput" ref={register} name="hue" />
+            </div>
+            <div className="container">
+              <label className="item">Light or dark?</label>
+              <input className="itemInput" ref={register} name="luminosity" />
+            </div>
+            <div className="container">
+              <label className="item">Enter a width</label>
+              <input className="itemInput" ref={register} name="width" />
+            </div>
+            <div className="container">
+              <label className="item">Enter a height</label>
+              <input className="itemInput" ref={register} name="height" />
+            </div>
+            <button className="button item">Submit Color</button>
           </div>
+        </form>
+
+        <div
+          className="colorBox"
+          style={{
+            backgroundColor: textColor,
+            height: boxHeight + 'em',
+            width: boxWidth + 'em',
+          }}
+        >
+          <p className="randomColorText">This is your random color</p>
         </div>
-        <p className="randomColorText" style={{ color: textColor }}>
-          THIS IS YOUR RANDOM COLOR!!!!!
-        </p>
       </header>
     </div>
   );
